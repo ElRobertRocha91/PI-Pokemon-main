dd76c993f8ec70c948901a22b5f03f2eadd909b0
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-04-23 21:01:29 -0300

    Front-End Style

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { getPokemonByName } from "../../redux/actions";
 import Paginado from "../paginado/Paginado";
+import style from "./SearchBar.module.css";
 
 //Creo mi función SearchBar para el input de busqueda:
 export default function SearchBar(){
@@ -31,8 +32,8 @@ export default function SearchBar(){
 
     //Renderizar el input y el button:
     return(
-        <div>
-            <form>
+        <div className={style.container}>
+            <form className={style.form}>
                 <input type="text" placeholder="Search by name..." onChange={(e) => handleInputChange(e)}  value={name}/>
                 <button type="submit" onClick={(e) => handleSubmit(e)}>Search</button>
             </form>
